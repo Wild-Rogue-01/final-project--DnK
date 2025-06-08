cb540b4c91f3f6e0915d5fececaa75efeedbde56
Wild-Rogue-01 <dashel.shearman@student.csd509j.net>
2025-06-08 16:14:45 -0700

    code final draft
    added rules, functions, and a simulation

--- main.ts
+++ main.ts
@@ -8,34 +8,47 @@ let gridToggle: boolean = false
 let cursor: Sprite = null
 let simToggle: boolean = false
 
-let worldSizeX: number = 15
-let worldSizeY: number = 15
+let worldBoxMax: number = 16
+let worldBoxXY: number = worldBoxMax
+let worldSizeX: number = worldBoxMax
+let worldSizeY: number = worldBoxMax
+
+let liveSurroundingNum: number = null
 
 //game update
 
-game.onUpdateInterval(100, function () {
+game.onUpdateInterval(400, function () {
     if (simToggle) {
         for (let y = 0; y < worldSizeY; y++) {
             for (let x = 0; x < worldSizeX; x++) {
+                liveSurroundingNum = 0
+                
+                if (tiles.tileAtLocationEquals(tiles.getTileLocation(x, y).getNeighboringLocation(CollisionDirection.Top), assets.tile`filled`)) {
+                    liveSurroundingNum++
+                }
+                if (tiles.tileAtLocationEquals(tiles.getTileLocation(x, y).getNeighboringLocation(CollisionDirection.Right), assets.tile`filled`)) {
+                    liveSurroundingNum++
+                }
+                if (tiles.tileAtLocationEquals(tiles.getTileLocation(x, y).getNeighboringLocation(CollisionDirection.Bottom), assets.tile`filled`)) {
+                    liveSurroundingNum++
+                }
+                if (tiles.tileAtLocationEquals(tiles.getTileLocation(x, y).getNeighboringLocation(CollisionDirection.Left), assets.tile`filled`)) {
+                    liveSurroundingNum++
+                }
 
-                tiles.setTileAt(tiles.getTileLocation(x, y), img`
-                    . . . . . . . . . . . . . . . .
-                    . . . . . . . . . . . . . . . .
-                    . . . . . . . . . . . . . . . .
-                    . . . . 2 . . . . . . . . . . .
-                    . . . . 2 2 . . . . 2 . . . . .
-                    . . . . . 2 . . . 2 . . . . . .
-                    . . . . . . 2 2 2 2 . . . . . .
-                    . . . . . . . 2 2 . . . . . . .
-                    . . . . . . 2 2 2 . . . . . . .
-                    . . . . . 2 . . . 2 . . . . . .
-                    . . . . 2 2 . . . . 2 . . . . .
-                    . . . . . . . . . . . 2 . . . .
-                    . . . . . . . . . . . . . . . .
-                    . . . . . . . . . . . . . . . .
-                    . . . . . . . . . . . . . . . .
-                    . . . . . . . . . . . . . . . .
-                `)
+                if (tiles.tileAtLocationEquals(tiles.getTileLocation(x, y), assets.tile`filled`)) {
+                    if (liveSurroundingNum < 2 || liveSurroundingNum > 3) {
+                        if (gridToggle) {
+                            tiles.setTileAt(tiles.getTileLocation(x, y), assets.tile`blank_gridtrue`)
+                        } else {
+                            tiles.setTileAt(tiles.getTileLocation(x, y), assets.tile`blank_gridfalse`)
+                        }
+                    }
+                } else {
+                    if (liveSurroundingNum == 3) {
+                        tiles.setTileAt(tiles.getTileLocation(x, y), assets.tile`filled`)
+                    }
+                }
             }
         }
     }
@@ -44,10 +57,48 @@ game.onUpdateInterval(100, function () {
 //event handlers
 
 //functions
+function controllerSync (upOdown: boolean) {
+    if(upOdown) {
+        if (controller.player1.isPressed(ControllerButton.Down)) {
+            upAndDown()
+        }
+    } else {
+        if (controller.player1.isPressed(ControllerButton.Up)) {
+            upAndDown()
+        }
+    }
+}
+function upAndDown () {
+    if (game.ask("Change border size?", "This can always be undone.")) {
+        worldBoxXY = 0
+        while (worldBoxXY < 3 || worldBoxXY > 16) {
+            worldBoxXY = game.askForNumber("Set world box size, 3 - 16", 2)
+        }
+        worldSizeX = worldBoxXY
+        worldSizeY = worldBoxXY
+
+        tiles.setCurrentTilemap(tilemap`world`)
+        tiles.placeOnTile(cursor, tiles.getTileLocation(1, 1))
+        
+        for (let y = worldBoxMax; y >= worldSizeY; y--) {
+            for (let x = 0; x <= worldBoxMax; x++) {
+                tiles.setWallAt(tiles.getTileLocation(x, y), true)
+                tiles.setTileAt(tiles.getTileLocation(x, y), assets.tile`wall`)
+                
+            }
+        }
+        for (let x = worldBoxMax; x >= worldSizeY; x--) {
+            for (let y = 0; y <= worldBoxMax; y++) {
+                tiles.setWallAt(tiles.getTileLocation(x, y), true)
+                tiles.setTileAt(tiles.getTileLocation(x, y), assets.tile`wall`)
+
+            }
+        }
+    }
+}
 
 //controller
-
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+controller.A.onEvent(ControllerButtonEvent.Released, function () {
     if (controller.player1.isPressed(ControllerButton.Left) && controller.player1.isPressed(ControllerButton.Right)) {
         if (game.ask("Toggle grid to " + !gridToggle + "?")) {
             gridToggle = !gridToggle
@@ -94,6 +145,13 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
             simToggle = !simToggle
         }
     }
+})
+
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    controllerSync(true)
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    controllerSync(false)
 })
 
 //main
